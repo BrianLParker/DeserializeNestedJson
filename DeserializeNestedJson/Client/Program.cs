203d1b6bd9007d2707f32b4f2a3549f601826396
Brian Parker <brian.parker@hotmail.com.au>
2023-04-21 22:00:09 +1000

    CODE RUB: Remove older poc code

--- DeserializeNestedJson/Client/Program.cs
+++ DeserializeNestedJson/Client/Program.cs
@@ -9,18 +9,18 @@ namespace DeserializeNestedJson
 {
     public class Program
     {
-public static async Task Main(string[] args)
-{
-    var builder = WebAssemblyHostBuilder.CreateDefault(args);
-    builder.RootComponents.Add<App>("#app");
-    builder.RootComponents.Add<HeadOutlet>("head::after");
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-    var host = builder.Build();
-    var httpClient = host.Services.GetService<HttpClient>();
-    var filters = await httpClient!.GetFromJsonAsync<Model>("filterOptions.json");
-    builder.Services.AddSingleton(filters!);
-    await builder.Build().RunAsync();
-}
+            var host = builder.Build();
+            var httpClient = host.Services.GetService<HttpClient>();
+            var filters = await httpClient!.GetFromJsonAsync<Model>("filterOptions.json");
+            builder.Services.AddSingleton(filters!);
+            await builder.Build().RunAsync();
+        }
     }
 }
---
+++ DeserializeNestedJson/Models/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace DeserializeNestedJson.Models
+{
+    public class Model
+    {
+        public IDictionary<string, string>? Text1 { get; set; }
+        public IDictionary<string, int>? Text2 { get; set; }
+        public IList<string>? Text3 { get; set; }
+        public IList<int>? Numbers { get; set; }
+    }
+}
